91b1f07bdd9844dad7782fe793d2082b8eca095f
liokim <liokimai02@gmail.com>
2025-05-21 17:16:15 +0900

    record delete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import Record from "./pages/Record";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
@@ -12,7 +11,6 @@ function App() {
         <>
             <nav>
                 <Link to="/">Home</Link> | <Link to="/login">Login</Link> |{" "}
-                <Link to="/record">Record</Link> |{" "}
                 <Link to="/signup">Signup</Link> |{" "}
                 <Link to="/diarylist">Diarylist</Link> |{" "}
                 <Link to="/logout">Logout</Link>
@@ -21,7 +19,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/record" element={<Record />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/diarylist" element={<Diarylist />} />
                 <Route path="/logout" element={<Logout />} />
